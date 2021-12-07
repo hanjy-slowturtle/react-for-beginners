93436540dbf2563a26eee32abb44c0f819e7292c
hanjy-slowturtle <h920915@gmail.com>
2021-12-07 22:25:55 +0900

    ~ #6.3 Recap

--- react-for-beginners-by-cra/src/App.js
+++ react-for-beginners-by-cra/src/App.js
@@ -2,13 +2,29 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("all the time");
   useEffect(() => {
     console.log("only once");
   }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Searching:", keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log("Counter:", counter);
+  }, [counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
