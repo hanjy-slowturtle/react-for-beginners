e49a3fd6b5f70621f5de05e6999f3e9484e93bd6
hanjy-slowturtle <h920915@gmail.com>
2021-12-08 21:53:55 +0900

    ~ #6.4 Cleanup

--- react-for-beginners-by-cra/src/App.js
+++ react-for-beginners-by-cra/src/App.js
@@ -1,32 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("all the time");
+function Hello() {
   useEffect(() => {
-    console.log("only once");
+    console.log("hi");
+    return () => console.group("bye");
   }, []);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5) {
-      console.log("Searching:", keyword);
-    }
-  }, [keyword]);
-  useEffect(() => {
-    console.log("Counter:", counter);
-  }, [counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
