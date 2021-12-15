780d1db42949c0488781a83c039663ded9e3ac31
hanjy-slowturtle <h920915@gmail.com>
2021-12-15 22:24:23 +0900

    ~ #7.6 Parameters

--- react-for-beginners-by-cra/src/routes/Detail.js
+++ react-for-beginners-by-cra/src/routes/Detail.js
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState(null);
+  const getMovie = async (id) => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    console.log(json);
+    setMovie(json.data.movie);
+    setLoading(false);
+  };
+  useEffect(() => getMovie(id), []);
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          <h1>Detail</h1>
+          <div>
+            <h3>{movie.title}</h3>
+            <img src={movie.large_cover_image} alt={movie.title} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
